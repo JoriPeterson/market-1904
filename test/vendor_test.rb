2b8b79c0ac59ccbc2390a1ae3b2fb4429b681abc
Jori Peterson <joripeterson@gmail.com>
2019-05-29 13:10:49 -0600

    Add vendor test

---
+++ test/vendor_test.rb
@@ -0,0 +1,29 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/vendor'
+
+class VendorTest < Minitest::Test
+
+  def setup
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+  end
+
+  def test_it_exists
+    assert_instance_of Vendor, @vendor
+  end
+
+  def test_it_has_attributes
+    assert_equal "Rocky Mountain Fresh", @vendor.name
+    expected = {}
+    assert_equal expected, @vendor.inventory
+  end
+
+  def test_it_can_check_stock
+    assert_equal ("Peaches", 30), @vendor.stock
+    assert_equal 30, @vendor.check_stock("Peaches")
+    assert_equal ("Peaches", 25), @vendor.stock
+    assert_equal 55, @vendor.check_stock("Peaches")
+    @vendor.stock("Tomatoes", 12)
+    expected = {"Peaches"=>55, "Tomatoes"=>12}
+    assert_equal expected, @vendor.inventory
+end
