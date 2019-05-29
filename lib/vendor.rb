1fe7daf38969d769a6b2558bf5143a11f46fdca3
Jori Peterson <joripeterson@gmail.com>
2019-05-29 13:36:58 -0600

    Add vendor class

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -3,6 +3,18 @@ class Vendor
 
   def initialize(name)
     @name = name
-    @inventory = {}
+    @inventory = Hash.new(0)
   end
+
+  def check_stock(food)
+    @inventory.each do |food, amount|
+      inventory[food] = amount
+    end
+    @inventory[food]
+  end
+
+  def stock(food, amount)
+    @inventory[food] += amount
+  end
+
 end
